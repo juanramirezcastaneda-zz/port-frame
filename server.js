e74bd106d3a8a0668b40b89c77803d1d58289190
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-02-28 19:09:44 +0000

    final configuration for prettier

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 'use-strict';
 
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import express from 'express';
 import { urlencoded, json } from 'body-parser';
-import { hidePoweredBy, frameguard, xssFilter, noSniff } from 'helmet';
+import {
+  hidePoweredBy,
+  frameguard,
+  xssFilter,
+  noSniff,
+} from 'helmet';
 import cors from 'cors';
 
 dotenv.config();
@@ -25,27 +30,30 @@ app.use(noSniff());
 
 // Not found middleware
 app.use((_req, _res, next) => {
-    return next({ status: 404, message: 'not found' })
+  return next({ status: 404, message: 'not found' });
 });
 
 // Error Handling middleware
 app.use((err, _req, res, _next) => {
-    let errCode, errMessage;
+  let errCode, errMessage;
 
-    if (err.errors) {
-        // mongoose validation error
-        errCode = 400 // bad request
-        const keys = Object.keys(err.errors);
-        // report the first validation error
-        errMessage = err.errors[keys[0]].message;
-    } else {
-        // generic or custom error
-        errCode = err.status || 500;
-        errMessage = err.message || 'Internal Server Error';
-    }
-    res.status(errCode).type('txt').send(errMessage);
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400; // bad request
+    const keys = Object.keys(err.errors);
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message;
+  } else {
+    // generic or custom error
+    errCode = err.status || 500;
+    errMessage = err.message || 'Internal Server Error';
+  }
+  res
+    .status(errCode)
+    .type('txt')
+    .send(errMessage);
 });
 
-app.listen(port, function () {
-    console.log(`Listening to requests on http://localhost:${port}`);
+app.listen(port, function() {
+  console.log(`Listening to requests on http://localhost:${port}`);
 });
