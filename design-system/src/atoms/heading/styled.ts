21790370d2beb0e675e5cda25a89486799501dc9
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-03-09 15:46:04 +0100

    add defaults for theme

--- design-system/src/atoms/heading/styled.ts
+++ design-system/src/atoms/heading/styled.ts
@@ -1,9 +1,10 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { HeadingProps } from './types';
+import theme from '~/theme';
 
 export const HeadingStyled = styled.h1<HeadingProps>`
   letter-spacing: normal;
-  font-family: Lato, sans-serif;
+  font-family: ${theme.fontFamilies.lato}, sans-serif;
   font-weight: normal;
   font-stretch: normal;
   font-style: normal;
---
+++ design-system/src/theme/index.ts
@@ -0,0 +1,9 @@
+import Colors from './colors';
+import FontFamilies from './fontFamilies';
+
+const theme = {
+  colors: Colors,
+  fontFamilies: FontFamilies,
+} as const;
+
+export default theme;
