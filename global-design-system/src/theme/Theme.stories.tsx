7c3c80604a921148a7e01196732e06c583734d61
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-03-12 15:27:23 +0100

    add first story

---
+++ global-design-system/src/theme/Theme.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import theme from '.';
+// import { withKnobs, select } from '@storybook/addon-knobs';
+import styled, { css } from 'styled-components';
+
+export default {
+  title: 'Theme',
+  //   decorators: [withKnobs],
+};
+
+const AllColorsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ColorInfo = styled.div`
+  background: #fff;
+  padding: 0.25rem;
+  text-align: center;
+`;
+
+const StyledDiv = styled.div<any>`
+  align-items: center;
+  background-color: ${p => p.color};
+  display: flex;
+  height: 125px;
+  justify-content: center;
+  margin-right: 1rem;
+  margin-bottom: 1rem;
+  width: 125px;
+`;
+
+export const AllColors = () => {
+  const colorKeys = Object.keys(theme.colors) as (keyof typeof theme.colors)[];
+  return (
+    <AllColorsContainer>
+      {colorKeys.map(key => (
+        <StyledDiv key={key} color={theme.colors[key]} spacing="0.5rem">
+          <ColorInfo>
+            <div>{key}</div>
+            <div>{theme.colors[key]}</div>
+          </ColorInfo>
+        </StyledDiv>
+      ))}
+    </AllColorsContainer>
+  );
+};
