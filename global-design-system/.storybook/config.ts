140007a966b0d25079c164d34b554e11fe958ed1
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-03-13 18:30:59 +0100

    config working with no typography

--- global-design-system/.storybook/config.ts
+++ global-design-system/.storybook/config.ts
@@ -1,6 +1,10 @@
-import { configure } from '@storybook/react';
+import { addDecorator, configure } from '@storybook/react';
+import { withTheme } from './theme';
+
 const req = require.context('../src', true, /\.stories\.tsx$/);
 function loadStories() {
+  addDecorator(withTheme);
+
   req.keys().forEach(req);
 }
 configure(loadStories, module);
--- global-design-system/src/global-styles/index.tsx
+++ global-design-system/src/global-styles/index.tsx
@@ -1,14 +1,14 @@
 import React, { Fragment, FC } from 'react';
 import { Reset } from './reset';
 import { BoxSizing } from './box-sizing';
-import { Typography } from './typography';
+// import { Typography } from './typography';
 
 export const GlobalStyles: FC = () => {
   return (
     <Fragment>
       <Reset />
       <BoxSizing />
-      <Typography />
+      {/* <Typography /> */}
     </Fragment>
   );
 };
