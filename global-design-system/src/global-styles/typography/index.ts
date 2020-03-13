28fa48d20c40e20cf4001c2ce5363f71d3e8f902
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-03-13 18:24:43 +0100

    add global style elements

---
+++ global-design-system/src/global-styles/typography/index.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+import theme from '~/theme/index';
+
+export const Typography = createGlobalStyle` html {        
+    font-family: ${theme.fontFamilies.lato}; 
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  a {
+    color: ${theme.colors.lightBlue};
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
