c6028f88d24a27df315c597404e7d05a34f3f6db
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-04-02 18:42:05 +0200

    add prettior to stage verify

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['git add', 'eslint'],
+  '*.{js,jsx,ts,tsx}': ['prettier --write', 'git add', 'eslint'],
   '*.json': ['prettier --write', 'git add'],
   '*.{ts,tsx}': ['stylelint'],
 };
