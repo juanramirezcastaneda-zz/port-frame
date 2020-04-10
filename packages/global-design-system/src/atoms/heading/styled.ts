df14206c7e316c6c6e8603652d188e376cb5cee7
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-04-10 21:44:50 +0200

    add basic styling for heading

--- packages/global-design-system/src/atoms/heading/styled.ts
+++ packages/global-design-system/src/atoms/heading/styled.ts
@@ -1,4 +1,14 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { HeadingProps } from './types';
 
-export const HeadingStyled = styled.div<HeadingProps>``;
+export const HeadingStyled = styled.h1<HeadingProps>`
+  letter-spacing: normal;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+
+  ${({ theme }) =>
+    css`
+      font-family: ${theme.fontFamilies.roboto};
+    `}
+`;
