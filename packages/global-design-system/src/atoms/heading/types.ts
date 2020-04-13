87a4f88ec7e73ef1e2d34f4ba19e5eb39aecb4d6
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-04-13 21:10:33 +0200

    add fontSize to type definition

--- packages/global-design-system/src/atoms/heading/types.ts
+++ packages/global-design-system/src/atoms/heading/types.ts
@@ -1,9 +1,11 @@
 import { HTMLAttributes } from 'react';
 import { HeadingAppearance } from './constants';
+import FontSizes from '~/theme/fontSizes';
 
 export interface HeadingComponentProps {
   asHeading?: typeof HeadingAppearance[number];
   className?: string;
+  fontSize?: keyof typeof FontSizes;
 }
 
 export type HeadingProps = HTMLAttributes<HTMLDivElement> & HeadingComponentProps;
