9fe7ecc17903384ea8561490fe879204cb5d9db8
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-04-14 20:45:31 +0200

    add color to implementation

--- packages/global-design-system/src/atoms/heading/internals.tsx
+++ packages/global-design-system/src/atoms/heading/internals.tsx
@@ -2,9 +2,9 @@ import React, { FC } from 'react';
 import { HeadingStyled } from './styled';
 import { HeadingProps } from './types';
 
-export const Heading: FC<HeadingProps> = ({ asHeading, fontSize, children }) => {
+export const Heading: FC<HeadingProps> = ({ asHeading, children, color, fontSize }) => {
   return (
-    <HeadingStyled as={asHeading} fontSize={fontSize}>
+    <HeadingStyled as={asHeading} color={color} fontSize={fontSize}>
       {children}
     </HeadingStyled>
   );
