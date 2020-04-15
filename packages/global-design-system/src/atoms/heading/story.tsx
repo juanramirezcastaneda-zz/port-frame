6086deed43db5650a52a4ef2ec3bd437266c1e6e
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-04-15 18:50:57 +0200

    include fontweight in history

--- packages/global-design-system/src/atoms/heading/story.tsx
+++ packages/global-design-system/src/atoms/heading/story.tsx
@@ -5,6 +5,7 @@ import { withKnobs, select, text } from '@storybook/addon-knobs';
 import { HeadingAppearance } from './constants';
 import FontSizes from '~/theme/fontSizes';
 import Colors from '~/theme/colors';
+import FontWeights from '~/theme/fontWeight';
 
 export default {
   title: 'atoms|Heading',
@@ -18,12 +19,14 @@ export default {
 
 const fontSizes = Object.keys(FontSizes) as (keyof typeof FontSizes)[];
 const colorOptions = Object.keys(Colors) as (keyof typeof Colors)[];
+const fontWeights = Object.keys(FontWeights) as (keyof typeof FontWeights)[];
 
 export const Configurable = () => (
   <Heading
     asHeading={select('As Heading', HeadingAppearance, 'h1')}
     color={select('Color', colorOptions, colorOptions[0])}
     fontSize={select('Font Size', fontSizes, '200')}
+    fontWeight={select('Font Weight', fontWeights, 'normal')}
   >
     {text('Body Text', 'H1')}
   </Heading>
