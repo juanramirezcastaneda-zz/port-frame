cf82118fa3f9e70da7e73319ac295440416b6cd0
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-04-15 18:40:16 +0200

    change name and remove default harcoded

--- packages/global-design-system/src/atoms/heading/styled.ts
+++ packages/global-design-system/src/atoms/heading/styled.ts
@@ -13,7 +13,7 @@ export const getHeadingFontSize = ({ fontSize }: HeadingProps) =>
     font-size: ${({ theme }) => theme.fontSizes[fontSize]};
   `;
 
-export const getFontWeight = ({ fontWeight }: HeadingProps) =>
+export const getHeadingFontWeight = ({ fontWeight }: HeadingProps) =>
   fontWeight &&
   css`
     font-weight: ${({ theme }) => theme.fontWeights[fontWeight]};
@@ -21,7 +21,6 @@ export const getFontWeight = ({ fontWeight }: HeadingProps) =>
 
 export const HeadingStyled = styled.h1<HeadingProps>`
   letter-spacing: normal;
-  font-weight: normal;
   font-stretch: normal;
   font-style: normal;
 
@@ -30,5 +29,7 @@ export const HeadingStyled = styled.h1<HeadingProps>`
       font-family: ${theme.fontFamilies.roboto};
     `}
 
+  ${getHeadingColor};
   ${getHeadingFontSize};
+  ${getHeadingFontWeight};
 `;
