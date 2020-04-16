7db7721b5cce157e39a260ca92fbf147421398b7
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-04-16 21:58:28 +0200

    Add constants file

---
+++ packages/global-design-system/src/atoms/icon/constants.ts
@@ -0,0 +1,19 @@
+import {
+  faBars,
+  faBookmark,
+  faChevronLeft,
+  faChevronDown,
+  faChevronRight,
+  faChevronUp,
+  faSearch,
+} from '@fortawesome/free-solid-svg-icons';
+
+export const Icons = {
+  bars: faBars,
+  bookmark: faBookmark,
+  chevronLeft: faChevronLeft,
+  chevronDown: faChevronDown,
+  chevronRight: faChevronRight,
+  chevronUp: faChevronUp,
+  search: faSearch,
+};
