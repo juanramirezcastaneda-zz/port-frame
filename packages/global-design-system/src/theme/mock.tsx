d995b1d4f58f9bc099257f47ba73615ecca1a927
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-04-28 18:14:58 +0200

    disable warning from ts

--- packages/global-design-system/src/theme/mock.tsx
+++ packages/global-design-system/src/theme/mock.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from '~/theme';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function MockThemeProvider({ children }: any) {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
