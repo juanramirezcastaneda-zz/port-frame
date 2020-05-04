ca4fe30be31f0c5f91ac64d781f4f6f0181ae1b0
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-05-04 19:10:54 +0200

    add themed extraction

--- packages/global-design-system/src/atoms/button/styled.ts
+++ packages/global-design-system/src/atoms/button/styled.ts
@@ -1,10 +1,10 @@
-import styled, { css } from 'styled-components';
-import { ButtonComponentProps } from './types';
+import styled, { DefaultTheme, ThemedStyledProps, css } from 'styled-components';
+import { ButtonProps } from './types';
 
-function getAppearance() {
+function getAppearanceStyles({ appearance, ghost, theme }: ThemedStyledProps<ButtonProps, DefaultTheme>) {
   return css``;
 }
 
-export const ButtonStyled = styled.button<ButtonComponentProps>`
-  ${getAppearance}
+export const ButtonStyled = styled.button<ButtonProps>`
+  ${getAppearanceStyles}
 `;
