7c7f5cbcf39414774d1784c4eb1b5a4edaa8657a
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-05-07 19:56:26 +0200

    include base styles

--- packages/global-design-system/src/atoms/button/styled.ts
+++ packages/global-design-system/src/atoms/button/styled.ts
@@ -2,7 +2,25 @@ import styled, { DefaultTheme, ThemedStyledProps, css } from 'styled-components'
 import { ButtonProps } from './types';
 
 function getAppearanceStyles({ appearance, ghost, theme }: ThemedStyledProps<ButtonProps, DefaultTheme>) {
-  return css``;
+  if (ghost) {
+    const baseStyles = css`
+      border: solid 1px;
+      background: transparent;
+      color: ${theme.colors.blackText};
+    `;
+    switch (appearance) {
+      case 'Primary':
+        return css`
+          ${baseStyles};
+        `;
+      default:
+        return css`
+          ${baseStyles};
+          border-color: ${theme.colors.whiteCore};
+          color: ${theme.colors.whiteCore};
+        `;
+    }
+  }
 }
 
 export const ButtonStyled = styled.button<ButtonProps>`
