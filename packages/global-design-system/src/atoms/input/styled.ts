6c436911160f88d64d7dd77f380b59e113c5d45e
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-05-08 18:56:06 +0200

    basic styled

--- packages/global-design-system/src/atoms/input/styled.ts
+++ packages/global-design-system/src/atoms/input/styled.ts
@@ -1,35 +1,11 @@
 import styled, { css } from 'styled-components';
 import { InputProps } from './types';
 
-export const LabelStyled = styled.label<Pick<InputProps, 'isInline'>>`
+export const LabelStyled = styled.label`
   white-space: pre;
-  ${(p) => {
-    if (!p.isInline) {
-      return css`
-        margin-bottom: ${({ theme }) => theme.margins.label};
-        margin-right: 0;
-      `;
-    } else {
-      return css`
-        align-self: center;
-        margin-right: ${({ theme }) => theme.margins.labelInline};
-        margin-bottom: 0;
-      `;
-    }
-  }};
 `;
 
-export const InputContainerStyled = styled.div`
-  width: 100%;
-  .form-group {
-    width: 100%;
-  }
-  .form-check-inline {
-    min-height: 38px;
-  }
-`;
-
-export const InputStyled = styled.div<Pick<InputProps, 'isInline'>>`
+export const InputStyled = styled.div<Pick<InputProps, 'isInline' | 'disabled'>>`
   width: 100%;
   display: flex;
   ${(p) => {
@@ -44,3 +20,20 @@ export const InputStyled = styled.div<Pick<InputProps, 'isInline'>>`
     }
   }}
 `;
+
+export const InputComponentStyled = styled.input``;
+
+// ${(p) => {
+//   if (!p.isInline) {
+//     return css`
+//       margin-bottom: ${({ theme }) => theme.margins.label};
+//       margin-right: 0;
+//     `;
+//   } else {
+//     return css`
+//       align-self: center;
+//       margin-right: ${({ theme }) => theme.margins.labelInline};
+//       margin-bottom: 0;
+//     `;
+//   }
+// }};
