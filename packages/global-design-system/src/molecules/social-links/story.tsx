4dda8ab0d15b2918caed9dccf82ecf355c39b00b
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-07-15 20:12:13 +0200

    add story implementation for social links

--- packages/global-design-system/src/molecules/social-links/story.tsx
+++ packages/global-design-system/src/molecules/social-links/story.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Readme from './readme.md';
 import { SocialLinks } from '.';
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
+import { Appearance } from './constants';
 
 export default {
   title: 'molecules|SocialLinks',
@@ -13,4 +14,12 @@ export default {
   },
 };
 
-export const Configurable = () => <SocialLinks />;
+export const Configurable = () => (
+  <SocialLinks
+    appearance={select('Appearance', Appearance, 'Light')}
+    linkedinUrl={text('LinkedIn URL', 'https://www.linkedin.com/in/juan-sebastian-ramirez-casta%C3%B1eda-8176a04b/')}
+    facebookUrl={text('Facebook URL', 'https://www.facebook.com/juan.s.ramirez.777')}
+    twitterUrl={text('Twitter URL', 'https://twitter.com/jsramirezc')}
+    youtubeUrl={text('Youtube URL', 'https://www.youtube.com/channel/UCbVNBFaRkgL-6kg92q1fsYw')}
+  />
+);
