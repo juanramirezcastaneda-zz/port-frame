5d58e271e8b6e262105ee0fd10e30b8942d2f5f7
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-07-15 20:10:14 +0200

    add internal implementation for social links

--- packages/global-design-system/src/molecules/social-links/internals.tsx
+++ packages/global-design-system/src/molecules/social-links/internals.tsx
@@ -1,7 +1,29 @@
-import React, { FC } from 'react';
-import { SocialLinksStyled } from './styled';
+import React from 'react';
+import { SocialLinksStyled, SocialLink } from './styled';
 import { SocialLinksProps } from './types';
+import { Icon } from '~/atoms/icon';
 
-export const SocialLinks: FC<SocialLinksProps> = ({ children }) => {
-  return <SocialLinksStyled>{children}</SocialLinksStyled>;
-};
+export function SocialLinks({
+  appearance = 'Light',
+  facebookUrl,
+  linkedinUrl,
+  twitterUrl,
+  youtubeUrl,
+}: SocialLinksProps) {
+  return (
+    <SocialLinksStyled>
+      <SocialLink href={linkedinUrl} appearance={appearance}>
+        <Icon icon="linkedin" />
+      </SocialLink>
+      <SocialLink href={twitterUrl} appearance={appearance}>
+        <Icon icon="twitter" />
+      </SocialLink>
+      <SocialLink href={facebookUrl} appearance={appearance}>
+        <Icon icon="facebook" />
+      </SocialLink>
+      <SocialLink href={youtubeUrl} appearance={appearance}>
+        <Icon icon="youtube" />
+      </SocialLink>
+    </SocialLinksStyled>
+  );
+}
