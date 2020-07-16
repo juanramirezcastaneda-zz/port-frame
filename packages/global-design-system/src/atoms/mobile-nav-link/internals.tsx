c349dfe75f9e3725a7ebcca6de4061d735d19a54
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-07-16 17:50:35 +0200

    connect props with styled implementation

--- packages/global-design-system/src/atoms/mobile-nav-link/internals.tsx
+++ packages/global-design-system/src/atoms/mobile-nav-link/internals.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { MobileNavLinkStyled } from './styled';
 import { MobileNavLinkProps } from './types';
 
-export function MobileNavLink({ children }: MobileNavLinkProps) {
-  return <MobileNavLinkStyled>{children}</MobileNavLinkStyled>;
+export function MobileNavLink({ className, children, href, sublink }: MobileNavLinkProps) {
+  return (
+    <MobileNavLinkStyled className={className} href={href} sublink={sublink}>
+      {children}
+    </MobileNavLinkStyled>
+  );
 }
