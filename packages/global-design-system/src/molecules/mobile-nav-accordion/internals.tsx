31d883299e216c3703f880ff026a8dbbcf613e45
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-07-20 23:06:29 +0200

    add internal implementation

--- packages/global-design-system/src/molecules/mobile-nav-accordion/internals.tsx
+++ packages/global-design-system/src/molecules/mobile-nav-accordion/internals.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
-import { MobileNavAccordionStyled } from './styled';
+import { MobileNavAccordionStyled, SubLinks, SubLinksContainer, IconLink, Header } from './styled';
 import { MobileNavAccordionProps } from './types';
+import { MobileNavLink } from '~/atoms/mobile-nav-link';
+import { useAccordion } from './hooks';
+import { animated } from 'react-spring';
+import { Icon } from '~/atoms/icon';
 
-export function MobileNavAccordion({ children }: MobileNavAccordionProps) {
-  return <MobileNavAccordionStyled>{children}</MobileNavAccordionStyled>;
+export function MobileNavAccordion({ title, url, children }: MobileNavAccordionProps) {
+  const { height, toggleAccordion, ref, isOpen } = useAccordion();
+
+  return (
+    <MobileNavAccordionStyled>
+      <Header>
+        <MobileNavLink href={url}>{title}</MobileNavLink>
+        <IconLink onClick={toggleAccordion} isOpen={isOpen}>
+          <Icon icon="chevronDown" />
+        </IconLink>
+      </Header>
+      <SubLinksContainer>
+        <animated.div style={{ height }}>
+          <SubLinks ref={ref}>{children}</SubLinks>
+        </animated.div>
+      </SubLinksContainer>
+    </MobileNavAccordionStyled>
+  );
 }
