0fc3d88d8e6815e9928730b18f4d0152c46cff71
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-07-20 23:06:50 +0200

    add styled for sublinks

--- packages/global-design-system/src/molecules/mobile-nav-accordion/styled.ts
+++ packages/global-design-system/src/molecules/mobile-nav-accordion/styled.ts
@@ -1,4 +1,38 @@
-import styled from 'styled-components';
-import { MobileNavAccordionProps } from './types';
+import styled, { css } from 'styled-components';
+import { MobileNavLinkStyled } from '~/atoms/mobile-nav-link';
 
-export const MobileNavAccordionStyled = styled.div<MobileNavAccordionProps>``;
+export const Header = styled.div`
+  display: flex;
+`;
+
+export const IconLink = styled.button<{ isOpen: boolean }>`
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 1rem;
+  margin-left: auto;
+  outline: none;
+  padding: 0 0.625rem;
+  transition: 0.2s transform;
+
+  ${(p) =>
+    p.isOpen &&
+    css`
+      transform: rotate(180deg);
+    `};
+`;
+
+export const MobileNavAccordionStyled = styled.div``;
+
+export const SubLinks = styled.div`
+  border-left: 2px solid ${({ theme }) => theme.colors.gray};
+
+  ${MobileNavLinkStyled} {
+    padding-left: ${({ theme }) => theme.space['600']};
+  }
+`;
+
+export const SubLinksContainer = styled.div<any>`
+  height: ${(p) => p.height};
+  overflow: hidden;
+`;
